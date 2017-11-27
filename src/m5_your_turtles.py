be4d6bf7c347a9289a7e41e1b7dd24657ef6fdec
riveraar <riveraar@rose-hulman.edu>
2017-11-27 16:42:06 -0500

    Added my name and code for two turtles making art in a loop.

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Angel Rivera.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,36 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+angel = rg.SimpleTurtle('turtle')
+angel.pen = rg.Pen('midnight blue', 3)
+angel.speed = 5
+
+drewe = rg.SimpleTurtle('turtle')
+drewe.pen = rg.Pen('pink', 3)
+drewe.speed = 10
+
+size = 100
+
+for k in range(8):
+    angel.draw_circle(size)
+    drewe.draw_square(size)
+
+    angel.pen_up()
+    angel.right(45)
+    angel.forward(10)
+    angel.left(45)
+    angel.pen_down()
+
+    drewe.pen_up()
+    drewe.right(45)
+    drewe.forward(10)
+    drewe.left(45)
+    drewe.pen_down()
+
+    size = size - 10
+
+window.close_on_mouse_click()
