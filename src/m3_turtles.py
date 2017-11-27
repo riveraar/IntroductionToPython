9d93a3fbcc5d86689865fb724c6119ee516c51ed
riveraar <riveraar@rose-hulman.edu>
2017-11-27 16:23:27 -0500

    Did my part on the turtles.

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -73,11 +73,14 @@ matt.speed = 10  # Faster
 matt.backward(50)
 matt.left(90)
 matt.forward(50)
+matt.left(90)
+matt.pen = rg.Pen('yellow', 10)
+matt.forward(50)
 
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +94,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +112,15 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+angel = rg.SimpleTurtle('turtle')
+angel.pen = rg.Pen('white', 50)
+angel.speed = 50
+angel.backward(100)
+angel.right(90)
+angel.forward(150)
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
